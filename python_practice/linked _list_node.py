2b28f43e984737ad08022e2dad6019e0f6ba4286
Duncan-Stout <68520045+Duncan-Stout@users.noreply.github.com>
2022-10-14 11:56:57 -0700

    added remove_node method

--- python_practice/linked _list_node.py
+++ python_practice/linked _list_node.py
@@ -36,12 +36,24 @@ class LinkedList:
       current_node = current_node.get_next_node()
     return string_list
          
-        
+  # remove_node method
+  def remove_node(self, value_to_remove):
+    current_node = self.get_head_node()
+    if current_node.get_value() == value_to_remove:
+        self.head_node = current_node.get_next_node()
+    else:
+        while current_node:
+            next_node = current_node.get_next_node()
+            if next_node.get_value()== value_to_remove:
+                current_node.set_next_node(next_node.get_next_node())
+                current_node = None
+            else:
+                current_node = next_node              
   
 
 # Test your code by uncommenting the statements below - did your list print to the terminal?
-ll = LinkedList(5)
-ll.insert_beginning(70)
-ll.insert_beginning(5675)
-ll.insert_beginning(90)
-print(ll.stringify_list())
+# ll = LinkedList(5)
+# ll.insert_beginning(70)
+# ll.insert_beginning(5675)
+# ll.insert_beginning(90)
+# print(ll.stringify_list())
